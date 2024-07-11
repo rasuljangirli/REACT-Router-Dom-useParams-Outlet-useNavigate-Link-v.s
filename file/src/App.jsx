66f45d2b75341d01a882e639f04180ec0460669b
rasuljangirli <127443671+rasuljangirli@users.noreply.github.com>
2024-07-11 17:31:02 +0400

    Update App.jsx

--- file/src/App.jsx
+++ file/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
-
+        
         <Route path="/about" element={<About />}>
           <Route path="employee" element={<Employee />} />
           <Route path="company" element={<Company />} />
